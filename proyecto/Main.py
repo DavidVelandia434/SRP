5e76292c2c0ef4cf4f4b8077150c04184e7f8b32
Yasminadriana <yasmina-trigost@unilibre.edu.co>
2025-09-29 09:41:09 -0500

    listo

--- proyecto/Main.py
+++ proyecto/Main.py
@@ -1,44 +1,46 @@
 print("SPR")
-generos[
-        acción,
-        Aventura,
-        Animacion,
-        Biografia,
-        Comedia,
-        Crimen,
-        Documental,
-        Drama,
-        Familiar,
-        Fantasia,
-        Cine-negro,
-        Concurso,
-        Historia,
-        Terror,
-        Musica,
-        Musical,
-        Misterio,
-        Noticias,
-        Reality,
-        Romance,
-        Ciencia-ficcion,
-        Corto,
-        Deporte,
-        Tertulia,
-        Suspense,
-        Belico,
-        Del-oeste]
+generos=[
+        "acción",
+        "Aventura",
+        "Animacion",
+        "Biografia",
+        "Comedia",
+        "Crimen",
+        "Documenta",
+        "Drama",
+        "Familiar",
+        "Fantasia",
+        "Cine-negro",
+        "Concurso",
+        "Historia",
+        "Terror",
+        "Musica",
+        "Musical",
+        "Misterio",
+        "Noticias",
+        "Reality",
+        "Romance",
+        "Ciencia-ficcion",
+        "Corto",
+        "Deporte",
+        "Tertulia",
+        "Suspense",
+        "Belico",
+        "Del-oeste"]
 
 
 print("----MENU GENERO-----")
-for i range(len(genero)):
-    print(f"{i+1}"{genero[1]}")
+for i in range(len(generos)):
+    print(f"{i+1}.{generos[i]}")
           
- opcion=int(input("seleccione genero"))         
+opcion=int(input("seleccione genero"))         
         
 if 1<=opcion<=len(generos):
     print("elegiste",generos[opcion-1])
 else:
-print("inavido.intente de nuevo")    
+    print("inavido.intente de nuevo")    
+
+  
 
 
 
